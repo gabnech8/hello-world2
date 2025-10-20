dea87e6d9b3130099bf505bc746ff4195b263848
Gabriel <tudorgabriel.nechita@studenti.unich.it>
2025-10-20 10:38:44 +0200

    Lab fork Java

--- src/saluto.java
+++ src/saluto.java
@@ -1,5 +1,15 @@
 public class saluto {
     public static void main(String[] args) {
         System.out.println("ciao mondo bellissimo");
+        String cleiLogo = """
+          _____ _      _____ ___ ___ 
+         / ____| |    | ____|_ _|_ _|
+        | |    | |    |  _|  | | | | 
+        | |___ | |___ | |___ | | | | 
+         \\____|_____| |_____|___|___|
+        """;
+
+        System.out.println(cleiLogo);
     }
 }
+
